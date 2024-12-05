aeb5f43349d1d2bc6654e04bf97e96c01cb7832a
Koukunn_ <testbing@outlook.jp>
2024-12-05 13:09:35 +0900

    update

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # ハッシュ化するパスワードの設定
-password = ""  # ここに検証するパスワードを入力
+password = "a"  # ここに検証するパスワードを入力
 hashed_password = pwd_context.hash(password)
 print(f"hashed password: {hashed_password}")
 
